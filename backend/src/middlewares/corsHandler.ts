f42ebf50354de101a33d3dbda7b63ed7dbea1559
30075199_adani <shubham.budhiraja@adani.com>
2024-12-07 11:00:18 +0530

    wip

--- backend/src/middlewares/corsHandler.ts
+++ backend/src/middlewares/corsHandler.ts
@@ -4,6 +4,12 @@ config();
 
 export const customCorsOptions = {
     origin: (origin: any, callback: any) => {
+        console.log(
+            "origin - >",
+            origin,
+            "allowed origins - >",
+            process.env.ALLOWED_ORIGINS
+        );
         if (process.env.ALLOWED_ORIGINS) {
             const allowedOrigins = process.env.ALLOWED_ORIGINS.split(" ");
 
